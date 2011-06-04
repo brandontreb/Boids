ad882fa51d719934d279fdd570e2c4f98e948d5c
Brandon Trebitowski <brandontreb@gmail.com>
2011-06-03 19:35:09 -0600

    Added multiple wanderers

--- main.lua
+++ main.lua
@@ -5,16 +5,26 @@ MAX_FORCE = 1.75
 MAX_SPEED = 2.0
 
 -- Start our wanderer in the center
-local loc = Vector2D:new(display.contentWidth / 2,display.contentWidth / 2)
-local wanderer = Boid:new(loc,MAX_FORCE,MAX_SPEED)
+
+local wanderers = {}
 
 function animate(event)	
-	wanderer:wander()
-	wanderer:run()
+	for i=1,#wanderers do
+		local wanderer = wanderers[i]
+		wanderer:wander()
+		wanderer:run()
+	end
 end
 
 function Main()
 	math.randomseed( os.time() )
+	
+	for i=1,10 do
+		local loc = Vector2D:new(display.contentWidth / 2,display.contentWidth / 2)
+		local wanderer = Boid:new(loc,MAX_FORCE,MAX_SPEED)
+		table.insert(wanderers,wanderer)
+	end
+	
 	Runtime:addEventListener( "enterFrame", animate );
 end
 
